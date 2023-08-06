e6dce54b788fd19dae94a8f3c1c8926d811dc083
mvl22 <martin@lucas-smith.co.uk>
2023-08-06 21:11:08 +0200

    Modal filters: Scale point size

--- src/lowtrafficneighbourhoods.js
+++ src/lowtrafficneighbourhoods.js
@@ -44,7 +44,15 @@ var lowtrafficneighbourhoods = (function ($) {
 		// LTNs - modal filters
 		modalfilters: {
 			apiCall: '/v2/advocacydata.modalfilters',
-			pointSize: 12,
+			pointSize: {
+				'base': 4,
+				'stops': [
+					[0, 4],
+					[12, 6],
+					[14, 12],
+					[18, 20]
+				]
+			},
 			pointColourApiField: 'colour',
 			zoomInitialMin: 10,
 			name: 'Modal filters',
